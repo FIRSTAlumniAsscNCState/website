ea85b512197b723e9d41dfcc97fde8f1c0b0a276
Sam Weaver <sam@samweaver.com>
2019-11-15 18:42:20 -0500

    Add Paypal donate button

--- src/components/MissionStatementHero.vue
+++ src/components/MissionStatementHero.vue
@@ -17,6 +17,24 @@
                     <i>FIRST</i>?
                 </a>
                 <a href="https://www.ncsu.edu" target="_blank" class="button">What is NC State?</a>
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                    <input type="hidden" name="cmd" value="_s-xclick" />
+                    <input type="hidden" name="hosted_button_id" value="MCBSG7EC7QRQU" />
+                    <input
+                        class="paypal-image"
+                        type="image"
+                        src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
+                        border="0"
+                        title="PayPal - The safer, easier way to pay online!"
+                        alt="Donate with PayPal button"
+                    />
+                    <button type="submit" class="button is-paypal">
+                        <span class="icon">
+                            <i class="fab fa-paypal"></i>
+                        </span>
+                        <span>Donate</span>
+                    </button>
+                </form>
             </div>
         </div>
         <div class="column is-paddingless is-hidden-touch">
@@ -36,4 +54,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.paypal-image {
+    display: none;
+}
 </style>
